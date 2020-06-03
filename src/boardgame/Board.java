accd0da787a4c28093fd5c4c06220643c653fec0
daniloalcantara <daniloalcantarabeltoso@gmail.com>
2020-06-03 14:58:27 -0300

    board exception and defensive program

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,39 +7,61 @@ public class Board {
 	private Piece[][] matriz_pieces;
 	
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro: deve existir ao menos 1 linha e 1 coluna");
+		}
 		this.rows = rows;
 		this.columns = columns;
-		matriz_pieces = new Piece[rows][columns];
-		
+		matriz_pieces = new Piece[rows][columns];		
 	}
 
 	public int getRows() {
 		return rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-	
 	public Piece piece (int row, int column) {
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posicao nao existe no tabuleiro");
+		}
 		return matriz_pieces[row][column];
 	}
 	
 	public Piece piece (Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao existe no tabuleiro");
+		}
 		return matriz_pieces [position.getRow()] [position.getColumn()];
 	}
+	
+	
 	// metodo  coloca uma peca no tabuleiro
 	public void placePiece(Piece piece, Position position) {
+		// testa se ja existe uma peca nesta posicao
+		if (thereIsAPiece(position)) {
+			throw new BoardException("ja existe uma peca nesta posicao" + position);
+		}
+		
 		matriz_pieces[position.getRow()][position.getColumn()]=piece;
 		piece.position = position;
+	}
+	
+	private boolean positionExists(int row, int column) {
+		return row >=0 && row < rows && column >=0 && column < columns;
+	}
+	
+	private boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn()) ;
+	}
+	
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posicao nao existe no tabuleiro");
+		}
+		return piece(position)!= null;
 	}
 
 }
